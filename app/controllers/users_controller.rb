aa87c6637ca4d38fb209e3c7413328eea0a5cce6
Jamezjaz <jamezjaz@gmail.com>
2020-07-01 18:23:33 -0500

    Creates  #form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,19 @@ class UsersController < ApplicationController
   # end
 
   def new
-
+    @user = User.new
   end
 
   def create
-    # @user = User.new(user_params)
-    # @user.save
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    @user.save
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
-    end
+    # if @user.save
+    #   redirect_to new_user_path
+    # else
+    #   render :new
+    # end
   end
 
 end
