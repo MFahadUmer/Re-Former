dd223bd8fefbc337a9be8cc90f64739a94f4a3ae
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-07-02 02:26:10 +0500

    Basic Html Forms

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, length: {maximum:40}, presence: true
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Please use the orignal format of Email" }
+  validates :password, length: {minimum: 8}
+end
