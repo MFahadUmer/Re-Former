0dbce34f727f319660382bd597726cbea33608b3
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-07-03 05:12:08 +0500

    Adds Error Message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   include UsersHelper
-  # def show
-  #   @user = User.find(params[:id])
-  # end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -10,13 +14,27 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+  
     # if @user.save
     #   redirect_to new_user_path
     # else
     #   render :new
     # end
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 end
