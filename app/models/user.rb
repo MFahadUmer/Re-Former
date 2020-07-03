bebfbbd0600f68a648848972ba88b2ac4414dfa8
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-07-03 05:28:02 +0500

    Resolve Rubocop Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # rubocop:disable Layout/LineLength
 
 class User < ApplicationRecord
-  validates :username, length: { minimum: 4, message: 'Minimum Characters are 4'}, presence: true
+  validates :username, length: { minimum: 4, message: 'Minimum Characters are 4' }, presence: true
   validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'Please use the orignal format of Email' }
   validates :password, length: { minimum: 8 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,15 +9,14 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+# rubocop:disable Style/NumericLiterals
 ActiveRecord::Schema.define(version: 2020_07_01_193328) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
+# rubocop:enable Style/NumericLiterals
